f9de8e4f0e1c15a432e98f7ec4cf5c15dfefa227
Paulo-Tonelotti <paulohenrique63664@gmail.com>
2023-03-31 19:26:19 -0300

    second commit

--- README.md
+++ README.md
@@ -4,25 +4,39 @@
 ### REQUISITOS
 - [X] Deve ser possivel criar uma conta
 - [X] Deve ser possivel buscar o extrato bancário do cliente
-- [] Deve ser possivel realizar um deposito
-- [] Deve ser possivel realizar um saque
-- [] Deve ser possivel buscar o extrato bancário do cliente por data
-- [] Deve ser possivel atualizar os dados da conta do cliente
-- [] Deve ser possivel obter dados da conta do cliente
-- [] Deve ser possivel deletar uma conta
+- [X] Deve ser possivel realizar um deposito
+- [X] Deve ser possivel realizar um saque
+- [X] Deve ser possivel buscar o extrato bancário do cliente por data
+- [X] Deve ser possivel atualizar os dados da conta do cliente
+- [X] Deve ser possivel obter dados da conta do cliente
+- [X] Deve ser possivel deletar uma conta
+- [X] Deve ser possivel retornar o balance
 
 
 ### Regras de negocio
 - [X] Não deve ser possivel cadastrar uma conta com CPF ja existente
 - [X] Não deve ser possivel buscar extrato em uma conta nao existente	
-- [] Não deve ser possivel fazer deposito em uma conta nao existente
-- [] Não deve ser possivel fazer um saque em uma conta não existente
-- [] Não deve ser possivel excluir uma conta não existente
-- [] Não deve ser possivel fazer um saque quando o saldo for insuficiente
+- [X] Não deve ser possivel fazer deposito em uma conta nao existente
+- [X] Não deve ser possivel fazer um saque em uma conta não existente
+- [X] Não deve ser possivel excluir uma conta não existente
+- [X] Não deve ser possivel fazer um saque quando o saldo for insuficiente
 
 
 ### Rotas
--POST /account - vai receber cpf e nome que vem do body da requisição, juntamente vai gerar um id para o cliente cadastrado para ser salvo
+-POST /account - cadastro de usuario
 
--GET /statement/ - vai receber um cpf dentro do header, consultará se existe algum customer com esse cpf cadastrado e retorna o objeto, caso não existir "Customer not found", caso existe retorna o statement do objeto, no caso extrato.
+-GET /statement/ - retorna operações feitas pelo usuario
 
+- POST /deposit - executa a operação de deposito na conta
+
+- POST /withdraw - executa a operação de retirada na conta
+
+- GET /statement/date - filtra as operações dos clientes por data
+
+- PUT /account - altera o nome do usuario
+
+- GET /account - retorna os dados do user/customer
+
+- DELETE /account - deleta um customer
+
+- GET /balance - retorna o saldo/balanco da conta do cliente
--- src/index.js
+++ src/index.js
@@ -1,43 +1,147 @@
 const express = require("express");
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-const constumers = []
+const costumers = [];
 
+//Middleware
+
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = costumers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).send({ error: "Customer not found" });
+  }
+
+  request.customer = customer; // Passa o objeto customer para a requisição
+
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === "credit"){
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
   // Verifica se o cpf já existe nos cadastros
-  const customerAlredyExists = 
-    constumers.some(customer => customer.cpf === cpf)
+  const customerAlredyExists = costumers.some(
+    (customer) => customer.cpf === cpf
+  );
 
   if (customerAlredyExists) {
     return response.status(400).send({ error: "Customer already exists" });
   }
 
-  constumers.push({
+  costumers.push({
     id: uuidv4(),
     cpf,
     name,
-    statement: []
+    statement: [],
   });
 
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
 
-  const costumer = constumers.find(costumer => costumer.cpf === cpf)
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
 
-  if(!costumer) {
-    return response.status(400).send({ error: "Customer not found" });
+  const statementOperation = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).send({ error: "Insufficient funds" });
   }
 
-  return response.json(costumer.statement)
-})
+  const statementOperation = {
+    amount,
+    createdAt: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.createdAt.toDateString() === new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  costumers.splice(customer, 1);
+
+  return response.status(200).json(customer);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement)
+
+  return response.json(balance)
+});
 
 app.listen(3333, () => console.log("Servidor rodando..."));
